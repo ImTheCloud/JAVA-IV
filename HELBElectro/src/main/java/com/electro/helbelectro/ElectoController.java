00e5947467ef832380da4bb55f197240ccec8afd
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-04-26 10:31:47 +0200

    test

--- HELBElectro/src/main/java/com/electro/helbelectro/ElectoController.java
+++ HELBElectro/src/main/java/com/electro/helbelectro/ElectoController.java
@@ -2,8 +2,14 @@ package com.electro.helbelectro;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class ElectoController {
     @FXML
@@ -13,6 +19,23 @@ public class ElectoController {
     protected void onButtonClicked(ActionEvent event) {
         Button button = (Button) event.getSource();
         String buttonText = button.getText();
-        place.setText("Place number : "+buttonText);
+
+        Stage modal = new Stage();
+        modal.initModality(Modality.APPLICATION_MODAL);
+
+        Label label = new Label("Place number : " + buttonText);
+
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e -> modal.close());
+
+        VBox vbox = new VBox(label, closeButton);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(20);
+        vbox.setPadding(new Insets(20));
+
+        Scene scene = new Scene(vbox);
+        modal.setScene(scene);
+        modal.showAndWait();
     }
+
 }
