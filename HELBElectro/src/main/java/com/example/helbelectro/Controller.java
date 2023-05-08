12d81a35e253646b0600422643b786acfb518e7b
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-05-09 00:16:42 +0200

    clear warning controller

--- HELBElectro/src/main/java/com/example/helbelectro/Controller.java
+++ HELBElectro/src/main/java/com/example/helbelectro/Controller.java
@@ -1,6 +1,5 @@
 package com.example.helbelectro;
 
-import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -11,12 +10,9 @@ import java.util.List;
 
 public class Controller {
     private static Controller instance = null;
-
-
     public static List<Object> productObjectList = new ArrayList<>();
     public static List<Product> productObjectListSorted = new ArrayList<>();
-    private static Timeline timeline = new Timeline();
-    static ObservableList<Object> componentObjectList = FXCollections.observableArrayList();
+    public static ObservableList<Object> componentObjectList = FXCollections.observableArrayList();
 
     public static Controller getInstance() {
         if (instance == null) {
@@ -25,41 +21,38 @@ public class Controller {
         return instance;
     }
 
-    public Object createComponent(String componentName, String[] values) {
+    public void createComponent(String componentName, String[] values) {
         // Vérifier si le nombre maximal de labels a été atteint
         if (componentObjectList.size() >= HELBElectro.number_lb_component) {
             System.out.println("Nombre maximal de composant atteint");
-            return null;
         }else{
             // Créer le composant
-            if (componentName.equals("Batterie")) {
-                String load = values[2];
-                ComponentBattery battery = new ComponentBattery(load);
-                componentObjectList.add(battery);
-                //componentNames.add("Batterie");//C-Type-1
-                System.out.println("Component Batterie : "+load);
-                return battery;
-            } else if (componentName.equals("Capteur")) {
-                String range = values[2];
-                String color = values[3];
-                ComponentSensor sensor = new ComponentSensor(range, color);
-                componentObjectList.add(sensor);
-                //componentNames.add("Capteur");
-                System.out.println("Component Capteur : "+range+", "+color);
-
-                return sensor;
-            } else if (componentName.equals("Moteur")) {
-                String power = values[2];
-                ComponentMotor motor = new ComponentMotor(power);
-                componentObjectList.add(motor);
-                // componentNames.add("Moteur");
-                System.out.println("Component Moteur : "+power);
-
-                return motor;
+            switch (componentName) {
+                case "Batterie" -> {
+                    String load = values[2];
+                    ComponentBattery battery = new ComponentBattery(load);
+                    componentObjectList.add(battery);
+                    //componentNames.add("Batterie");//C-Type-1
+                    System.out.println("Component Batterie : " + load);
+                }
+                case "Capteur" -> {
+                    String range = values[2];
+                    String color = values[3];
+                    ComponentSensor sensor = new ComponentSensor(range, color);
+                    componentObjectList.add(sensor);
+                    //componentNames.add("Capteur");
+                    System.out.println("Component Capteur : " + range + ", " + color);
+                }
+                case "Moteur" -> {
+                    String power = values[2];
+                    ComponentMotor motor = new ComponentMotor(power);
+                    componentObjectList.add(motor);
+                    // componentNames.add("Moteur");
+                    System.out.println("Component Moteur : " + power);
+                }
             }
 
         }
-        return null;
     }
 
 
@@ -86,15 +79,14 @@ public class Controller {
 
 
             if (hasAllComponents) {
-                int manufacturingDuration = product.getManufacturingDuration();
+               // int manufacturingDuration = product.getManufacturingDuration();
                 //System.out.println("Attente de " + manufacturingDuration + " secondes avant de fabriquer " + product.getClass().getSimpleName());
                 // Utilisation de Timeline pour la pause
 
 //                try {
 //                    Thread.sleep(manufacturingDuration*1000);
-                Product newProduct = product;
-                productObjectList.add(newProduct);
-                System.out.println(newProduct.getClass().getSimpleName() + " créé ");
+                productObjectList.add(product);
+                System.out.println(product.getClass().getSimpleName() + " créé ");
 //                } catch (InterruptedException e) {
 //                    throw new RuntimeException(e);
 //                }
@@ -122,7 +114,7 @@ public class Controller {
 
 
 
-    public static List<Product> addProductList() {
+    public static void addProductList() {
         productObjectListSorted.add(new ProductBattery());
         productObjectListSorted.add(new ProductSensor());
         productObjectListSorted.add(new ProductMotor());
@@ -130,40 +122,36 @@ public class Controller {
         productObjectListSorted.add(new ProductAlarm());
         productObjectListSorted.add(new ProductDrone());
         productObjectListSorted.add(new ProductRobot());
-        return productObjectListSorted;
     }
-    public static List<Product> getSortedProductListByTime() {
+    public static void getSortedProductListByTime() {
         addProductList();
         productObjectListSorted.sort(Comparator.comparing(Product::getManufacturingDuration));
-//        System.out.println("Product list sorted by time :");
-//        for (Product product : productObjectListSorted) {
-//            System.out.println(product.getClass().getSimpleName() +
-//                    " time : " + product.getManufacturingDuration());
-//        }
-//        System.out.println("\n");
-        return productObjectListSorted;
+        System.out.println("Product list sorted by time :");
+        for (Product product : productObjectListSorted) {
+            System.out.println(product.getClass().getSimpleName() +
+                    " time : " + product.getManufacturingDuration());
+        }
+        System.out.println("\n");
     }
-    public static List<Product> getSortedProductListByScore() {
+    public static void getSortedProductListByScore() {
         addProductList();
         productObjectListSorted.sort(Comparator.comparing(Product::getEcoScore));
-//        System.out.println("Product list sorted by score :");
-//
-//        for (Product product : productObjectListSorted) {
-//            System.out.println(product.getClass().getSimpleName() +
-//                    ", score : " + product.getEcoScore());
-//        }
-        return productObjectListSorted;
+        System.out.println("Product list sorted by score :");
+
+        for (Product product : productObjectListSorted) {
+            System.out.println(product.getClass().getSimpleName() +
+                    ", score : " + product.getEcoScore());
+        }
     }
-    public static List<Product> getSortedProductListByPrice() {
+    public static void getSortedProductListByPrice() {
         addProductList();
         productObjectListSorted.sort(Comparator.comparing(Product::getSellingPrice));
         Collections.reverse(productObjectListSorted);
-//        System.out.println("Product list sorted by Price :");
-//        for (Product product : productObjectListSorted) {
-//            System.out.println(product.getClass().getSimpleName() +
-//                    ", Price : " + product.getSellingPrice());
-//        }
-        return productObjectListSorted;
+        System.out.println("Product list sorted by Price :");
+        for (Product product : productObjectListSorted) {
+            System.out.println(product.getClass().getSimpleName() +
+                    ", Price : " + product.getSellingPrice());
+        }
     }
 
 }
