4dcc2cee7ddb6613da9521774033ac5cceabe928
claudiuppdc <100693885+ImTheCloud@users.noreply.github.com>
2023-05-09 00:08:06 +0200

    update main

--- HELBElectro/src/main/java/com/example/helbelectro/HELBElectro.java
+++ HELBElectro/src/main/java/com/example/helbelectro/HELBElectro.java
@@ -20,49 +20,27 @@ import javafx.stage.Stage;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
-
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-
-
 public class HELBElectro extends Application {
 
     private final VBox areaComponent = new VBox();
     private final GridPane areaProduct = new GridPane();
-    private Button bt_productFInish = null,sellButton = null,statsButton = null;
-    private Timeline timelineComponent;
-    private Timeline timelineChoiceOpti = new Timeline();
-    private ComboBox<String> optiComboBox = new ComboBox<>();
+    private final Timeline timelineChoiceOpti = new Timeline();
+    private final ComboBox<String> optiComboBox = new ComboBox<>();
     private List<Label> componentLabelsList;
     public static final int number_lb_component =8;
-    private final int lb_component_height = 42;
-    private final int lb_component_with = 183;
-    private final int size_row = 4;
-    private final int size_col = 3;
-    private final int bt_product_height = 73;
-    private final int bt_product_with = 138;
-
-    private final int WIDTH = 776;
-    private final int HEIGHT = 538;
-    private final String BACKGROUND_COLOR = "#282F76";
-    private final String BORDER_COLOR = "white";
-    private final int BORDER_WIDTH = 4;
+    private final int widthScene = 776;
+    private final int heightScene = 538;
+    private final String border_color = "white";
     private final String GRID_BACKGROUND_COLOR = "#626786";
-    private final int GRID_PADDING = 20;
-    private final int COMPONENT_SPACING = 10;
-    private final int OPTI_SPACING = 10;
-    private final String OPTI_BOX_BACKGROUND_COLOR = "#626786";
-    private final String OPTI_LABEL_TEXT = "Opti : ";
-    private final int OPTI_LABEL_FONT_SIZE = 20;
     private final Color OPTI_LABEL_COLOR = Color.WHITE;
-    private final String DEFAULT_OPTI_VALUE = "Diverse";
-    private final ObservableList<String> OPTI_LIST = FXCollections.observableArrayList("Cost", "Time", "Score", "Diverse");
 
     @Override
     public void start(Stage stage) {
-        HBox root = createRoot();
-        Scene scene = new Scene(root, WIDTH + 74, HEIGHT + 42);
+        HBox screen = createRoot();
+        Scene scene = new Scene(screen, widthScene, heightScene);
         stage.setTitle("HELBElectro");
         stage.setScene(scene);
         stage.show();
@@ -72,51 +50,53 @@ public class HELBElectro extends Application {
     }
 
     private HBox createRoot() {
-        HBox root = new HBox();
-        root.setAlignment(Pos.CENTER);
-        root.setPrefSize(WIDTH, HEIGHT);
-        root.setSpacing(20.0);
-        root.setStyle(String.format("-fx-background-color: %s; -fx-border-color: %s; -fx-border-width: %dpx;", BACKGROUND_COLOR, BORDER_COLOR, BORDER_WIDTH));
-        root.setPadding(new Insets(20, 20, 20, 20));
+        HBox screen = new HBox();
+        screen.setAlignment(Pos.CENTER);
+        screen.setPrefSize(widthScene, heightScene);
+        screen.setSpacing(10.0);
+        String backgrund_color = "#282F76";
+        int border_width = 4;
+        screen.setStyle(String.format("-fx-background-color: %s; -fx-border-color: %s; -fx-border-width: %dpx;", backgrund_color, border_color, border_width));
+        screen.setPadding(new Insets(20, 20, 20, 20));
         GridPane grid = gd_araProduct();
         VBox vbox = vb_areaComponent();
-        root.getChildren().addAll(grid, vbox);
+        screen.getChildren().addAll(grid, vbox);
 
-        return root;
+        return screen;
     }
 
     private GridPane gd_araProduct() {
-        areaProduct.setId("areaProduct");
-        areaProduct.setHgap(20);
-        areaProduct.setVgap(20);
-        areaProduct.setStyle(String.format("-fx-border-color: %s; -fx-border-width: 2px; -fx-background-color: %s;", BORDER_COLOR, GRID_BACKGROUND_COLOR));
-        areaProduct.setPadding(new Insets(GRID_PADDING, GRID_PADDING, GRID_PADDING, GRID_PADDING));
-        Label label = new Label();
-        label.setId("place");
-        areaProduct.add(label, 0, 1, 3, 1);
+        areaProduct.setHgap(10);
+        areaProduct.setVgap(10);
+        areaProduct.setStyle(String.format("-fx-border-color: %s; -fx-border-width: 2px; -fx-background-color: %s;", border_color, GRID_BACKGROUND_COLOR));
+        areaProduct.setPadding(new Insets(20, 20, 20, 20));
         initializeProductArea();
         return areaProduct;
     }
 
     public void initializeProductArea() {
-        for (int i = 0; i < size_col; i++) {
+        int size_colGrid = 3;
+        for (int i = 0; i < size_colGrid; i++) {
             ColumnConstraints column = new ColumnConstraints();
             column.setHgrow(Priority.ALWAYS); // agrandir
             areaProduct.getColumnConstraints().add(column);
         }
-        for (int i = 0; i < size_row; i++) {
+        int size_rowGrid = 4;
+        for (int i = 0; i < size_rowGrid; i++) {
             RowConstraints row = new RowConstraints();
             row.setVgrow(Priority.ALWAYS);
             areaProduct.getRowConstraints().add(row);
         }
 
-        for (int i = 0; i < size_row; i++) {
-            for (int j = 0; j < size_col; j++) {
+        for (int i = 0; i < size_rowGrid; i++) {
+            for (int j = 0; j < size_colGrid; j++) {
                 Button button = new Button();
+                int bt_product_with = 138;
+                int bt_product_height = 73;
                 button.setPrefSize(bt_product_with, bt_product_height);
                 button.setStyle("-fx-background-color: white;");
                 button.setOnAction(this::onComponentClicked);
-                if (i == size_row-1 && j == size_col-1) {
+                if (i == size_rowGrid -1 && j == size_colGrid -1) {
                     // derniere case de la grid pas de bouton
                     // comme dans l'interface du prof
                     continue;
@@ -127,34 +107,27 @@ public class HELBElectro extends Application {
     }
 
     public void setButtonProduct() {
-        List<Product> productList = new ArrayList<>();
-        for (Object obj : Factory.productObjectList) {
-            if (obj instanceof Product) {
-                productList.add((Product) obj);
-            }
-        }
-
         int index = 0;
-        for (Node node : areaProduct.getChildren()) {
-            if (node instanceof Button) {
-                if (index >= productList.size()) {
+        for (Node node : areaProduct.getChildren()) { // node pour parcouri la grid
+            if (node instanceof Button setButton) { // on peux donner un nom au bouton deja ici
+                if (index >= Factory.productObjectList.size()) { // index out of bound si on ne verifie pas l'index
                     break;
                 }
-                Product product = productList.get(index);
-                Button button = (Button) node;
-                button.setUserData(product);
-                button.setText(product.getnameForP());
-                button.setStyle("-fx-background-color: " + product.getColor() + ";");
+                // vu que les bouton change de valeur il faut bien faire en sorte
+                // que ce soit ces bouton qui change sans en crer d'autre
+                Product product = (Product) Factory.productObjectList.get(index);
+                setButton.setUserData(product);
+                setButton.setText(product.getnameForP());
+                setButton.setStyle("-fx-background-color: " + product.getColor() + ";");
                 index++;
             }
         }
-        Factory.productObjectList.clear();
+        Factory.productObjectList.clear(); // on vide sinon a chaque ajoute ca recommence pour tout
     }
 
     private VBox vb_areaComponent() {
-        areaComponent.setSpacing(COMPONENT_SPACING);
-        areaComponent.setId("areaComponent");
-        areaComponent.setStyle(String.format("-fx-border-color: %s; -fx-border-width: 2px; -fx-padding: 10px; -fx-background-color: %s;", BORDER_COLOR, GRID_BACKGROUND_COLOR));
+        areaComponent.setSpacing(10);
+        areaComponent.setStyle(String.format("-fx-border-color: %s; -fx-border-width: 2px; -fx-padding: 10px; -fx-background-color: %s;", border_color, GRID_BACKGROUND_COLOR));
         HBox optiBox = createOptiBox();
         areaComponent.getChildren().addAll(optiBox, new VBox());
         initializeComponentArea();
@@ -165,6 +138,8 @@ public class HELBElectro extends Application {
         componentLabelsList = new ArrayList<>();
         for (int i = 0; i < number_lb_component; i++) {
             Label label = new Label();
+            int lb_component_with = 183;
+            int lb_component_height = 42;
             label.setPrefSize(lb_component_with, lb_component_height);
             label.setId("component" + i);
             label.setAlignment(Pos.CENTER);
@@ -176,7 +151,7 @@ public class HELBElectro extends Application {
     }
 
     public void setLabelComponents() {
-        timelineComponent = new Timeline(new KeyFrame(Duration.seconds(0.1), event -> {
+        Timeline timelineComponent = new Timeline(new KeyFrame(Duration.seconds(0.1), event -> {
             try {
                 Parser.getInstance().parseSimulationFile();
             } catch (FileNotFoundException e) {
@@ -223,87 +198,83 @@ public class HELBElectro extends Application {
     }
 
     private Label getComponentLabel(int index) {
-        if (index < 1 || index > componentLabelsList.size()) {
-        }
         return componentLabelsList.get(index - 1);
     }
 
     private HBox createOptiBox() {
         HBox optiBox = new HBox();
-        optiBox.setStyle(String.format("-fx-border-color: %s; -fx-border-width: 2px; -fx-padding: 10px; -fx-background-color: %s;", BORDER_COLOR, OPTI_BOX_BACKGROUND_COLOR));
-        optiBox.setSpacing(OPTI_SPACING);
-        Label optiLabel = new Label(OPTI_LABEL_TEXT);
+        optiBox.setStyle(String.format("-fx-border-color: %s; -fx-border-width: 2px; -fx-padding: 10px; -fx-background-color: %s;", border_color, "#626786"));
+        optiBox.setSpacing(10);
+        Label optiLabel = new Label("Opti : ");
         optiLabel.setTextFill(OPTI_LABEL_COLOR);
-
-
-        optiComboBox.setId("cb_opti");
         optiComboBox.setValue("Diverse");
         ObservableList<String> optiList = FXCollections.observableArrayList("Cost", "Time", "Score", "Diverse");
         optiComboBox.setItems(optiList);
 
         optiBox.getChildren().addAll(optiLabel, optiComboBox);
-       getChoiceOpti();
+        getChoiceOpti();
         return optiBox;
     }
 
     public void getChoiceOpti() {
         optiComboBox.setOnAction(event -> {
             String selectedItem = optiComboBox.getSelectionModel().getSelectedItem();
-            if (selectedItem.equals("Time")) {
-                Factory.getSortedProductListByTime();
-                timelineChoiceOpti.stop();
-                timelineChoiceOpti.getKeyFrames().clear();
-                timelineChoiceOpti.getKeyFrames().add(new KeyFrame(Duration.seconds(3), e -> {
-                    Factory.createProduct();
-                     setButtonProduct();
-                }));
-                timelineChoiceOpti.setCycleCount(Animation.INDEFINITE);
-                timelineChoiceOpti.play();
-            } else if (selectedItem.equals("Cost")) {
-                Factory.getSortedProductListByPrice();
-                timelineChoiceOpti.stop();
-                timelineChoiceOpti.getKeyFrames().clear();
-                timelineChoiceOpti.getKeyFrames().add(new KeyFrame(Duration.seconds(3), e -> {
-                    Factory.createProduct();
-                     setButtonProduct();
-                }));
-                timelineChoiceOpti.setCycleCount(Animation.INDEFINITE);
-                timelineChoiceOpti.play();
-            } else if (selectedItem.equals("Score")) {
-                Factory.getSortedProductListByScore();
-                timelineChoiceOpti.stop();
-                timelineChoiceOpti.getKeyFrames().clear();
-                timelineChoiceOpti.getKeyFrames().add(new KeyFrame(Duration.seconds(3), e -> {
-                    Factory.createProduct();
-                       setButtonProduct();
-                }));
-                timelineChoiceOpti.setCycleCount(Animation.INDEFINITE);
-                timelineChoiceOpti.play();
-            } else if (selectedItem.equals("Diverse")) {
-                //Factory.getSortedProductListByScore();
-                //setButtonProduct();
-                timelineChoiceOpti.stop();
-                timelineChoiceOpti.getKeyFrames().clear();
-                timelineChoiceOpti.getKeyFrames().add(new KeyFrame(Duration.seconds(3), e -> {
-                    //Factory.createProduct();
-                }));
-                timelineChoiceOpti.setCycleCount(Animation.INDEFINITE);
-                timelineChoiceOpti.play();
+            switch (selectedItem) {
+                case "Time" -> {
+                    Factory.getSortedProductListByTime();
+                    timelineChoiceOpti.stop();
+                    timelineChoiceOpti.getKeyFrames().clear();
+                    timelineChoiceOpti.getKeyFrames().add(new KeyFrame(Duration.seconds(3), e -> {
+                        Factory.createProduct();
+                        setButtonProduct();
+                    }));
+                    timelineChoiceOpti.setCycleCount(Animation.INDEFINITE);
+                    timelineChoiceOpti.play();
+                }
+                case "Cost" -> {
+                    Factory.getSortedProductListByPrice();
+                    timelineChoiceOpti.stop();
+                    timelineChoiceOpti.getKeyFrames().clear();
+                    timelineChoiceOpti.getKeyFrames().add(new KeyFrame(Duration.seconds(3), e -> {
+                        Factory.createProduct();
+                        setButtonProduct();
+                    }));
+                    timelineChoiceOpti.setCycleCount(Animation.INDEFINITE);
+                    timelineChoiceOpti.play();
+                }
+                case "Score" -> {
+                    Factory.getSortedProductListByScore();
+                    timelineChoiceOpti.stop();
+                    timelineChoiceOpti.getKeyFrames().clear();
+                    timelineChoiceOpti.getKeyFrames().add(new KeyFrame(Duration.seconds(3), e -> {
+                        Factory.createProduct();
+                        setButtonProduct();
+                    }));
+                    timelineChoiceOpti.setCycleCount(Animation.INDEFINITE);
+                    timelineChoiceOpti.play();
+                }
+                case "Diverse" -> {
+                    timelineChoiceOpti.stop();
+                    timelineChoiceOpti.getKeyFrames().clear();
+                    timelineChoiceOpti.getKeyFrames().add(new KeyFrame(Duration.seconds(3), e -> {
+                    }));
+                    timelineChoiceOpti.setCycleCount(Animation.INDEFINITE);
+                    timelineChoiceOpti.play();
+                }
             }
         });
     }
-
-
+    
     protected void onComponentClicked(ActionEvent event) {
         // Pour savoir quel bouton a été cliqué
-        bt_productFInish = (Button) event.getSource();
+        Button bt_productFinish = (Button) event.getSource();
         // Obtient l'indice de la ligne et de la colonne du bouton dans la grille
-        int rowIndex = GridPane.getRowIndex(bt_productFInish);
-        int columnIndex = GridPane.getColumnIndex(bt_productFInish);
+        int rowIndex = GridPane.getRowIndex(bt_productFinish);
+        int columnIndex = GridPane.getColumnIndex(bt_productFinish);
         // Obtient les attributs du produit associé au bouton
         Label emplacements = new Label("Emplacements (" + rowIndex + ", " + columnIndex + ")");
 
-        Product product = (Product) bt_productFInish.getUserData();
+        Product product = (Product) bt_productFinish.getUserData();
 
 
         // Crée une nouvelle fenêtre
@@ -315,12 +286,12 @@ public class HELBElectro extends Application {
         Label price = new Label("Prix : " + product.getSellingPrice()+" euros");
         Label ecoScore = new Label("Eco-Score : " + product.getEcoScore());
 
-        statsButton = new Button("Voir les statistiques de cet emplacement");
+        Button statsButton = new Button("Voir les statistiques de cet emplacement");
         statsButton.setStyle("-fx-background-color:  #3f7ad9; -fx-text-fill: white;");
         statsButton.setOnAction(e -> {
         });
 
-        sellButton = new Button("Vendre produit");
+        Button sellButton = new Button("Vendre produit");
         sellButton.setStyle("-fx-background-color: #0b6517; -fx-text-fill: white;");
         sellButton.setOnAction(e -> {
 
@@ -333,10 +304,9 @@ public class HELBElectro extends Application {
             alert.setContentText("Le produit a été vendu !");
             alert.showAndWait();
             modal.close();
-            bt_productFInish.setStyle("-fx-background-color: white;");
-            bt_productFInish.setText("");
+            bt_productFinish.setStyle("-fx-background-color: white;");
+            bt_productFinish.setText("");
         });
-
         // UNE VBox pour ajouter les labels
         VBox vbox = new VBox(emplacements,type, price, ecoScore, statsButton, sellButton);
         vbox.setAlignment(Pos.CENTER);
@@ -346,7 +316,4 @@ public class HELBElectro extends Application {
         modal.setScene(new Scene(vbox, 400, 350));
         modal.showAndWait();
     }
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
