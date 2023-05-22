9677e2471f3e3b51e5cfdadf588c4c30173ae669
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2023-05-23 00:17:46 +0200

    commentaire

--- HELBElectro/src/main/java/com/example/helbelectro/conponent/Component.java
+++ HELBElectro/src/main/java/com/example/helbelectro/conponent/Component.java
@@ -1,7 +1,6 @@
 package com.example.helbelectro.conponent;
 
 public interface Component {
-    // nom et couleur pour l'interface graphique pour chaque composant
     String getName();
     String getColor();
 
